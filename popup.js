16a379b4bf6c000bf413793a996c1e456e0a92c2
Lucky Joshi <luckyjoshi524@gmail.com>
2025-01-27 19:14:57 +0530

    Local Update

--- popup.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    chrome.storage.local.get('adsSkipped', (data) => {
-      document.getElementById('adsSkippedCount').textContent = data.adsSkipped || '0';
-    });
-  });
-  
